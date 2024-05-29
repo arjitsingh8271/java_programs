b5d8c7e7a78b7080fe85b1b52039fe8dd3519569
arjitsingh8271 <arjitkumarsingh@gmail.com>
2024-05-29 19:53:36 +0530

    update

--- Assignment/asgmt_08_package.java
+++ Assignment/asgmt_08_package.java
@@ -1,3 +1,10 @@
+/*
+Write a Java program which will contain the user-defined package Calculator with all 4 basic
+arithmetic operations in a class and another class in package will contain operations like
+Square and Square Root (use Math.sqrt()) method.
+*/
+
+
 import myPack.PackBasicCalculator;
 import myPack.PackAdvancedCalculator;
 
