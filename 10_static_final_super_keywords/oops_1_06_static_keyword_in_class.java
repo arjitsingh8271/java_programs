96d6c70305fe45f634201f240e5461528a5f72be
arjitsingh8271 <singharjitkumar@gmail.com>
2024-08-25 20:18:58 +0530

    update

--- 10_static_final_super_keywords/oops_1_06_static_keyword_in_class.java
+++ 10_static_final_super_keywords/oops_1_06_static_keyword_in_class.java
@@ -6,7 +6,9 @@ we need to declare the class members static.
 
 class A
 {
-	public static int num = 2;
+	public static int sNum = 1;
+
+	int num;
 
 	public static void display()
 	{
@@ -18,11 +20,26 @@ class oops_1_06_static_keyword_in_class
 {
 	public static void main (String[] args)
 	{
-		System.out.println(A.num);	// 2
-		A.display();				// display method called
+		System.out.println(A.sNum);		// 1
+		A.display();					// display method called
 
-		A.num = 5;
-		System.out.println(A.num);	// 5
+		A.sNum = 2;
+		System.out.println(A.sNum);		// 2
+
+		System.out.println("------------------");
+		
+		A a = new A();
+		a.num = 10;
+		a.sNum = 3;
+		System.out.println(a.num);		// 10 	created a new memory
+		System.out.println(a.sNum);		// 3	share the same memory location
+
+		A b = new A();
+		System.out.println(b.num);		// 0 	created a new memory
+		System.out.println(b.sNum);		// 3 	share the same memory location
+
+
+
 
 	}
 }
